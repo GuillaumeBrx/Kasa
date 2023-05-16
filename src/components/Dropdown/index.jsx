3096a898fd5887671d3ae18731528ba18f43518b
GuillaumeBrx <121470768+GuillaumeBrx@users.noreply.github.com>
2023-05-16 18:17:41 +0200

    modified dropdown components

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -3,7 +3,6 @@ import colors from "../../utils/styles/colors";
 import { ReactComponent as OpenDropdown } from "../../assets/ArrowDropdown/OpenDropdown.svg";
 import { ReactComponent as CloseDropdown } from "../../assets/ArrowDropdown/CloseDropdown.svg";
 import { useState } from "react";
-import AboutDatas from "../../utils/datas/AboutDatas";
 
 const ButtonWrapper = styled.div`
   display: flex;
@@ -11,7 +10,7 @@ const ButtonWrapper = styled.div`
   justify-content: center;
   align-items: center;
   padding-inline: 108px;
-  padding-block: 33px 46px;
+  padding-block: 15px;
   gap: 31px;
 `;
 
@@ -43,32 +42,28 @@ const Description = styled.p`
   padding: 36px 27px 19px 18px;
 `;
 
-function AboutDropdown() {
-  const [isOpen, setOpen] = useState([false, false, false, false]);
+function Dropdown({ title, description }) {
+  const [isOpen, setOpen] = useState(false);
 
-  const handleClick = (index) => {
-    const buttonOpen = [...isOpen];
-    buttonOpen[index] = !buttonOpen[index];
-    setOpen(buttonOpen);
+  const handleClick = () => {
+    setOpen(!isOpen);
   };
 
   return (
     <ButtonWrapper>
-      {AboutDatas.map(({ title, description }, index) => (
-        <div key={index} style={{ width: "100%" }}>
-          <StyledButton onClick={() => handleClick(index)}>
-            {title}
-            <i>{isOpen[index] ? <CloseDropdown /> : <OpenDropdown />}</i>
-          </StyledButton>
-          {isOpen[index] && (
-            <TextWrapper>
-              <Description>{description}</Description>
-            </TextWrapper>
-          )}
-        </div>
-      ))}
+      <div style={{ width: "100%" }}>
+        <StyledButton onClick={handleClick}>
+          {title}
+          <i>{isOpen ? <CloseDropdown /> : <OpenDropdown />}</i>
+        </StyledButton>
+        {isOpen && (
+          <TextWrapper>
+            <Description>{description}</Description>
+          </TextWrapper>
+        )}
+      </div>
     </ButtonWrapper>
   );
 }
 
-export default AboutDropdown;
+export default Dropdown;
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Banner from "../../components/Banner";
-import AboutDropdown from "../../components/AboutDropdown";
+import Dropdown from "../../components/Dropdown";
+import AboutDatas from "../../utils/datas/AboutDatas";
 
 const StyledMain = styled.main`
   padding-inline: 100px;
@@ -10,7 +11,13 @@ function About() {
   return (
     <StyledMain>
       <Banner />
-      <AboutDropdown />
+      {AboutDatas.map((data, index) => (
+        <Dropdown
+          key={index}
+          title={data.title}
+          description={data.description}
+        />
+      ))}
     </StyledMain>
   );
 }
